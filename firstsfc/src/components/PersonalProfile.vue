eadeb9483a090ba13eab1e61f59480f34e5468b4
outdeath <cggarcia@student.apc.edu.ph>
2026-01-16 09:04:15 +0800

    thing added task 03 jhfdsbjfn

---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container">
+ 
+<header> <h1> My Personal Profile Web page - Charlize Jacee G. Garcia </h1> </header>
+<main>
+  <section class="main-content">
+     <div> About Me </div>
+     <div> ( Do not include birthdate / age / contact information) </div>
+     <div> Education / Achievements </div>
+     <div> Graduated in Far Eastern University (FEU) Alabang </div>
+     <div> Course </div>
+     <div> IT Experience </div>
+     <div> basic html and css </div>
+     <div> Hobbies & Interests </div>
+     <div> i like to play with my touys </div>
+     <div> Goals In Life / Dream </div>
+     <div> Picture Gallery </div>
+</p>
+ 
+ 
+  
+</body>
+</html>
